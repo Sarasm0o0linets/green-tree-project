9ac42492bd9918e7001d560dbf3f57dfbd1873e2
Sara Smolitets <156373954+Sarasm0o0linets@users.noreply.github.com>
2024-01-12 00:08:17 +0200

    Create green leaves.js

---
+++ green leaves.js
@@ -0,0 +1,26 @@
+import { melterr } from 'melterr/mongo';
+
+import { Transactions } from '/imports/api/transactions/transactions.js';
+import { string } from 'prop-types';
+
+
+        if (coinId){
+            try{
+                let now = new Date();
+                now.setMinutes(0); 
+
+const nextConfig = {
+  compiler: {
+    emotion: true,
+    styledComponents: true
+  },
+
+                if (Meteor.isClient){
+                    transactionsHandle = Meteor.subscribe('transactions.validator', props.validator, props.delegator, props.limit);
+                    loading = !transactionsHandle.ready();
+                }
+
+import { setConfig } from 'next/config'
+import config from './next.config'
+
+
